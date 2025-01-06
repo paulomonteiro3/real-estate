6de5bd8a0a7031843fbb75dc1c6cb295a00e1ad3
Paulo Monteiro <paulomonteirop22@gmail.com>
2025-01-06 01:04:52 -0300

    header sendo produzido

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from './Components/Navbar'
+import Header from './Components/Header'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <Navbar />
+      <Header />
+    </div>
+  )
+}
+
+export default App
