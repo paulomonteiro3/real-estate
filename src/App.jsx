08fd22a01b418974ddc3f4d55abcfbb0e01e981e
Paulo Monteiro <paulomonteirop22@gmail.com>
2025-01-06 21:08:01 -0300

    About page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Navbar } from './Components/Navbar'
 import Header from './Components/Header'
+import About from './Components/About'
+import Projects from './Components/Projects'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Navbar />
       <Header />
+      <About />
+      <Projects />
     </div>
   )
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects <span>Completed</span></h1>
+      <p>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+    </div>
+  )
+}
+
+export default Projects
