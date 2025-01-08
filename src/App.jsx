b6499077107aca924514c5c00821ac2b2ef9f13c
Paulo Monteiro <paulomonteirop22@gmail.com>
2025-01-08 01:12:05 -0300

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Navbar } from './Components/Navbar'
-import Header from './Components/Header'
-import About from './Components/About'
-import Projects from './Components/Projects'
+import React from "react";
+import { Navbar } from "./Components/Navbar";
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Testimonials from "./Components/Testimonials";
+import Contact from "./Components/Contact";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div className='w-full overflow-hidden'>
-      <Navbar />
+    <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
